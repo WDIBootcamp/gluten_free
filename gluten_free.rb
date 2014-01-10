88111b92d89793302924514eedea7594c947f40c
steclaudinodaffara <ste.daff@gmail.com>
2014-01-09 22:40:16 -0800

    raised the error

--- gluten_free.rb
+++ gluten_free.rb
@@ -1,3 +1,4 @@
+require 'pry'
 #Title: Gluten Free
  
 #Prerequisites:
@@ -11,24 +12,75 @@
  
 "======================================================================"
  
-# Create a Person class. A person will have a stomach and allergies
-# Create a method that allows the person to eat and add arrays of food to their stomachs
-# If a food array contains a known allergy reject the food.
- 
+class AllergyError < ArgumentError
+	# attr_reader :object
+
+	# def initialize(object)
+	# 	@object = object
+	# end
+
+end
+
 class Person
+
+	# A person will have a name, a stomach and allergies
+	# attr_accesor allows these items to be both written and read
+	attr_accessor :name, :allergies
+	def initialize (name, allergies)
+		@stomach = []
+		@allergies = allergies
+		@meal = []
+		@name = name
+	end
+
+	# Create a method that allows the person to eat and add arrays of food to their stomachs
+	# If a food array contains a known allergy reject the food.
+	def feed (meal)
+		#add the array of the food to their stomach
+		@meal.each	do |ingredient| 
+			if ingredient == @allergies
+				begin 
+					raise AllergyError.new("You cant eat that! 
+						You are allergic to #{@allergies}")
+				# rescue AllergyError => error
+				# 	puts "You are allergic to #{error}"
+				end
+
+			else 
+			@stomach << @meal
+			end
+		end
+	end
+
+	def food 
+		puts "What would you like to eat? [1]Pizza, [2]Scallops or [3]Water?"
+
+		# food options:
+		# maybe turn these into a hash for clear purposes
+		food_choice = gets.chomp
+		pizza = ["cheese", "gluten", "tomatoes"]
+		pan_seared_scallops = ["scallops", "lemons", "pasta", "olive oil"]
+		water = ["h", "h", "o"]
+
+		case food_choice
+		when "1" then @meal = pizza 
+		when "2" then @meal = pan_seared_scallops
+		when "3" then @meal = water
+		end
+
+		return @meal 
+	end
+
 end
  
+
+
 # Create a Person named Chris. Chris is allergic to gluten.
 # Create a Person named Beth. Beth is allergic to scallops.
 # Feed them the following foods
  
-pizza = ["cheese", "gluten", "tomatoes"]
-pan_seared_scallops = ["scallops", "lemons", "pasta", "olive oil"]
-water = ["h", "h", "o"]
- 
 # When a person attempts to eat a food they are allergic to, raise a custom exception
 # For example:  AllergyError
- 
  
 # Bonus: When a person attempts to eat a food they are allergic to,
 #        ... remove ALL the food from the person's stomach before raising the exception
